1a7205b801379ae81b54577fd766a03cd66cba34
Mac <yurika_mota@outlook.com>
2026-03-17 16:28:26 -0300

    add teste_3.dart

---
+++ flutter_application_1/lib/teste_3.dart
@@ -0,0 +1,11 @@
+int add (int a, int b) {
+  return a + b;
+}
+
+void main() {
+  int result = add(3, 5);
+  print('The result is: $result');
+}
+
+// mac@Macs-Air lib % dart teste_3.dart
+// The result is: 8
